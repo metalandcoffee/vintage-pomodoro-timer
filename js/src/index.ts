a3537ce9d5eeb3946ebf1c7a4273ef7aed9b528a
Ebonie Butler <ebonie@yikesinc.com>
2023-08-12 13:52:27 -0400

    lint

--- js/src/index.ts
+++ js/src/index.ts
@@ -1,6 +1,13 @@
-import { addMinutes, setMilliseconds, intervalToDuration, addSeconds, differenceInSeconds } from 'date-fns';
+import {
+	addMinutes,
+	setMilliseconds,
+	intervalToDuration,
+	addSeconds,
+	differenceInSeconds,
+} from 'date-fns';
+import { tmi } from 'tmi.js';
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener( 'DOMContentLoaded', function () {
 	const SECOND_IN_MILLISECONDS: number = 1000;
 
 	const PomodoroTimer = {
@@ -10,12 +17,12 @@ document.addEventListener('DOMContentLoaded', function () {
 			timer: null,
 		},
 		init(): void {
-			const btn = document.getElementById('start');
-			const mins = document.getElementById('minutes');
-			const secs = document.getElementById('seconds');
+			const btn = document.getElementById( 'start' );
+			const mins = document.getElementById( 'minutes' );
+			const secs = document.getElementById( 'seconds' );
 
 			// Type guard. Check if null or undefined. Abort if so.
-			if (!btn || !mins || !secs) {
+			if ( ! btn || ! mins || ! secs ) {
 				return;
 			}
 			const btnEl = btn as HTMLButtonElement;
@@ -24,87 +31,85 @@ document.addEventListener('DOMContentLoaded', function () {
 
 			btnEl.addEventListener(
 				'click',
-				this.toggleButton.bind({
-					countdownStart: this.countdownStart.bind(this),
+				this.toggleButton.bind( {
+					countdownStart: this.countdownStart.bind( this ),
 					btnEl,
 					minsEl,
 					secsEl,
 					settings: this.settings,
-				})
+				} )
 			);
-			minsEl.addEventListener('input', this.updateMins.bind(this));
-			secsEl.addEventListener('input', this.updateSecs.bind(this));
+			minsEl.addEventListener( 'input', this.updateMins.bind( this ) );
+			secsEl.addEventListener( 'input', this.updateSecs.bind( this ) );
 
-			const settingsBtn = document.querySelector('.settings');
+			const settingsBtn = document.querySelector( '.settings' );
 			// Type guard. Check if null or undefined. Abort if so.
-			if (!settingsBtn) {
+			if ( ! settingsBtn ) {
 				return;
 			}
 			const settingsBtnEl = settingsBtn as HTMLButtonElement;
 			settingsBtnEl.addEventListener(
 				'click',
-				this.editTimer.bind(this)
+				this.editTimer.bind( this )
 			);
 		},
-		updateMins(e): void {
+		updateMins( e ): void {
 			const next: HTMLInputElement = e.target;
-			this.settings.minutesLimit = isNaN(next.valueAsNumber) ? 0 : next.valueAsNumber;
+			this.settings.minutesLimit = isNaN( next.valueAsNumber )
+				? 0
+				: next.valueAsNumber;
 		},
-		updateSecs(e): void {
+		updateSecs( e ): void {
 			const next: HTMLInputElement = e.target;
-			this.settings.secondsLimit = isNaN(next.valueAsNumber) ? 0 : next.valueAsNumber;
+			this.settings.secondsLimit = isNaN( next.valueAsNumber )
+				? 0
+				: next.valueAsNumber;
 		},
 		toggleButton(): void {
-			let btnText: string | undefined =
+			const btnText: string | undefined =
 				this.btnEl.innerText.toLowerCase();
 
-			if ('start' === btnText) {
+			if ( 'start' === btnText ) {
 				this.countdownStart();
 				this.btnEl.innerText = 'stop';
 			} else {
 				// Stop countdown and reset values.
-				clearInterval(this.settings.timer);
+				clearInterval( this.settings.timer );
 				this.btnEl.innerText = 'start';
 				this.minsEl.value = this.settings.minutesLimit
 					.toString()
-					.padStart(2, '0');
+					.padStart( 2, '0' );
 				this.secsEl.value = this.settings.secondsLimit
 					.toString()
-					.padStart(2, '0');
+					.padStart( 2, '0' );
 			}
 		},
-		countdownStart: function (): void {
+		countdownStart(): void {
 			let currentTime = new Date();
-			currentTime = setMilliseconds(currentTime, 0);
-			let endTime = addMinutes(
-				currentTime,
-				this.settings.minutesLimit
-			);
-			endTime = addSeconds(
-				endTime,
-				this.settings.secondsLimit
-			);
+			currentTime = setMilliseconds( currentTime, 0 );
+			let endTime = addMinutes( currentTime, this.settings.minutesLimit );
+			endTime = addSeconds( endTime, this.settings.secondsLimit );
 
-			const remainder = differenceInSeconds(endTime, currentTime);
+			const remainder = differenceInSeconds( endTime, currentTime );
 
-			const circle = document.getElementById('circle');
+			const circle = document.getElementById( 'circle' );
 			// Type guard.
-			if (!circle) {
+			if ( ! circle ) {
 				return;
 			}
 			const circleEl = circle as HTMLOrSVGElement;
 			circle.style.strokeDashoffset = '1596';
-			circle.style.animation = 'stroke ' + String(remainder) + 's linear forwards';
+			circle.style.animation =
+				'stroke ' + String( remainder ) + 's linear forwards';
 			/* These properties need to be set via JS on click */
 			/* stroke-dashoffset: 1596; */
 			/* animation: stroke 10s ease-out forwards; */
 
-
-			const mins = document.getElementById('minutes');
-			const secs = document.getElementById('seconds');
+			const mins = document.getElementById( 'minutes' );
+			const secs = document.getElementById( 'seconds' );
 
 			// Type guard and current time check.
-			if (!secs || !mins) {
+			if ( ! secs || ! mins ) {
 				return;
 			}
 			const minsEl = mins as HTMLInputElement;
@@ -112,45 +117,45 @@ document.addEventListener('DOMContentLoaded', function () {
 
 			function timerCallback() {
 				currentTime = new Date();
-				currentTime = setMilliseconds(currentTime, 0);
+				currentTime = setMilliseconds( currentTime, 0 );
 
-				let remainingTime = intervalToDuration({
+				const remainingTime = intervalToDuration( {
 					start: currentTime,
 					end: endTime,
-				});
+				} );
 
 				const remainingMins = remainingTime.minutes as number;
 				const remainingSecs = remainingTime.seconds as number;
 
 				// Render updated values.
-				minsEl.value = remainingMins.toString().padStart(2, '0');
-				secsEl.value = remainingSecs.toString().padStart(2, '0');
+				minsEl.value = remainingMins.toString().padStart( 2, '0' );
+				secsEl.value = remainingSecs.toString().padStart( 2, '0' );
 
-				if (!remainingTime.minutes && !remainingTime.seconds) {
-					clearInterval(this.settings.timer);
+				if ( ! remainingTime.minutes && ! remainingTime.seconds ) {
+					clearInterval( this.settings.timer );
 				}
 			}
 
 			this.settings.timer = setInterval(
-				timerCallback.bind(this),
+				timerCallback.bind( this ),
 				SECOND_IN_MILLISECONDS
 			);
 		},
-		editTimer: function () {
-			const mins = document.getElementById('minutes');
-			const secs = document.getElementById('seconds');
+		editTimer() {
+			const mins = document.getElementById( 'minutes' );
+			const secs = document.getElementById( 'seconds' );
 
 			// Type guard and current time check.
-			if (!secs || !mins) {
+			if ( ! secs || ! mins ) {
 				return;
 			}
 			const minsEl = mins as HTMLInputElement;
 			const secsEl = secs as HTMLInputElement;
 
-			minsEl.removeAttribute('disabled');
-			secsEl.removeAttribute('disabled');
+			minsEl.removeAttribute( 'disabled' );
+			secsEl.removeAttribute( 'disabled' );
 		},
 	};
 
 	PomodoroTimer.init();
-});
+} );
